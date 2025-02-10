ba9197784d0d34f2e155ef28a0349ca9c2511230
dennisalmendarez <dennisyassid2212@hotmail.com>
2025-02-10 14:13:23 -0500

    small fix

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: "stores API",
         description: "API for managing stores.",
     },
-    host: "https://final-project-cb6l.onrender.com",
+    host: "final-project-cb6l.onrender.com",
     schemes: ['https'],
 };
 
